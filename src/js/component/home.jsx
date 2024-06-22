245330c17e8664535585e4088766451a3017c891
uAngelSalcedo <162602493+uAngelSalcedo@users.noreply.github.com>
2024-06-22 23:06:41 +0000

    todolist api

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import ItemList from "./itemList";
 
-//create your first component
 const Home = () => {
+
+	const [itemList, setItemList] = useState("")
+	const [list, setList] = useState([])
+
+	async function getList() {
+		try {
+			const response = await fetch("https://playground.4geeks.com/todo/users/uAngelsalcedo")
+			const data = await response.json()
+			console.log(data);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	function addListItem(e) {
+
+		if (e.key == "Enter") {
+			setList([...list, itemList])
+			console.log(list);
+
+		}
+
+	}
+
+	function deletItemList(elementToDelete) {
+		const newList = list.filter((item) => item !== elementToDelete)
+		setList(newList)
+	}
+
+	useEffect(() =>{
+		getList()
+	},[])
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container">
+			<div className="card">
+				<div className="card-header">
+					<h1 className="fs-1 opacity-25">Todos</h1>
+				</div>
+				<input onChange={(event) => setItemList(event.target.value)} onKeyDown={(e) => addListItem(e)} placeholder="Nueva Tarea"></input>
+				<ul className="list-group list-group-flush">
+					<ItemList delete={deletItemList} list={list} />
+				</ul>
+			</div>
 		</div>
 	);
 };
